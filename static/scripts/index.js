660b759c5a574fb20f21155498a8208f0f718af3
jleoj <84648928+jleoj@users.noreply.github.com>
2025-11-03 03:15:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/scripts/index.js
@@ -0,0 +1,61 @@
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+const input = document.querySelector("input");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  window.navigator.serviceWorker
+    .register("/static/uv.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      const url = search(address.value, searchEngine.value);
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "edu.html";
+    });
+});
+
+
+function go(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("/static/uv.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "edu.html";
+      });
+  }
+  function isUrl(val = "") {
+    if (
+      /^http(s?):\/\//.test(val) ||
+      (val.includes(".") && val.substr(0, 1) !== " ")
+    )
+      return true;
+    return false;
+  }
